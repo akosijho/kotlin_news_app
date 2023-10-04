b52b649620902f7a37cc2834d0762a7d160b5a2b
akosijho <jhobertpanerio@gmail.com>
2023-10-04 16:11:10 +0800

    Create navigation

--- app/src/main/java/com/jhobert/news/ui/MainActivity.kt
+++ app/src/main/java/com/jhobert/news/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.jhobert.news.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.jhobert.news.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private  lateinit var newsNavHostFragment: Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        newsNavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.newsHostFragment) as NavHostFragment
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
